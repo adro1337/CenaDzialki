b1dd8feb29b3afe69224a726d48ad594f5e18d54
adro <95049259+adro1337@users.noreply.github.com>
2023-08-13 16:00:33 +0200

    Create cena.py

---
+++ cena.py
@@ -0,0 +1,6 @@
+#Proste obliczanie wartości działki na podstawie podania powierzchni i ceny za m2
+
+powierzchnia = int (input ("wpisz powierzchnie działki: "))
+cenaZam2 = int ( input ("wpisz cene za m2: "))
+obliczenie = (powierzchnia * cenaZam2)
+print ("cena działki to: ", obliczenie, "PLN")
